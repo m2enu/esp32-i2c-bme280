8cc411f946fca6e75981bfbbaf90cdadf3991ae9
m2enu <m2enu915vogel@gmail.com>
2017-07-19 21:43:42 +0900

    add including bmp280.h, calling device init function

--- main/main.c
+++ main/main.c
@@ -4,12 +4,19 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "driver/gpio.h"
+#include "bme280.h"
 
 /* <!-- app_main {{{1 -->
  * @brief main function
  */
 void app_main(void)
 {
+    struct bme280_dev dev = {
+        .id = BME280_I2C_ADDR_SEC,
+        .interface = BME280_I2C_INTF
+    };
+    int8_t ret = bme280_init(&dev);
+
     gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
     int level = 0;
     while (true) {
